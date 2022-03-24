17c813808e36f03dbe4dd589066fd488be24a9ec
Taylor Blunt <tblunt@debian-BULLSEYE-live-builder-AMD64>
2022-03-23 21:15:51 -0700

    Added heap struct and min_heap function definitions

--- holes.h
+++ holes.h
@@ -9,4 +9,19 @@ typedef struct process {
     struct process * proc_arr;
 } process;
 
+struct Heap{
+    process *arr;
+    int count;
+    int capacity;
+    int heap_type; // for min heap , 1 for max heap
+};
+typedef struct Heap Heap;
+
 int inputChecker(int argc, char * argv[]);
+
+Heap *CreateHeap(int capacity,int heap_type);
+void insert(Heap *h, process thread);
+void heapify_bottom_top(Heap *h,int index);
+void heapify_top_bottom(Heap *h, int parent_node);
+process PopMin(Heap *h);
+void print(Heap *h);
