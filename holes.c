8da704cf869185e3c5af596acda671a8b66a63cc
taylor-dnblunt <taylor.dnblunt@gmail.com>
2022-03-24 20:55:10 -0700

    Made a linked list and added a head to the list

--- holes.c
+++ holes.c
@@ -27,13 +27,13 @@ int main(int argc, char * argv[]) {
     printf("Num Lines = %d\n", numLines);
     fclose(inFile);
 
-    process * pptr;//Allocate an array of 20 processes
-    process proc_list;
-    proc_list.proc_arr = malloc(sizeof(struct process) * numLines);
-    pptr = &proc_list;
-    pptr->numProcs = numLines;
+    proc_list * processes = malloc(sizeof(struct proc_list)); //Initial list that holds all procs
+    sim * ms = (sim *)malloc(sizeof(struct sim)); //Memory sim struct
+    ms->space_rem = MEMMAX;
+    processes->proc_arr = malloc(sizeof(struct process) * numLines);
+    processes->numProcs = numLines;
     
-
+    printf("Here?\n");
     const char s[2] = " ";
     char * token;
     int cnt = 0;
@@ -44,25 +44,45 @@ int main(int argc, char * argv[]) {
         process p;
         token = strtok(line, s);//pid
         p.pid = atoi(token);
+        p.currentQ = p.pid; //Set current q to its process number then deal as time goes on
         token = strtok(NULL, s);//memchunk
         p.memchunk = atoi(token);
-        pptr->proc_arr[cnt] = p;
+        processes->proc_arr[cnt] = p;
         cnt++;
     }
     fclose(inFile);
     printf("\n");
 
-    for (int i = 0; i < pptr->numProcs; i++) {
-        printf("Process pid %d needs %d memory\n", pptr->proc_arr[i].pid, pptr->proc_arr[i].memchunk);
+    for (int i = 0; i < processes->numProcs; i++) {
+        printf("Process pid %d needs %d memory\n", processes->proc_arr[i].pid, processes->proc_arr[i].memchunk);
     }
 
-    Heap * q = CreateHeap(pptr->numProcs, 0);
+    Heap * q = CreateHeap(processes->numProcs, 0);
     
-    for(int i = 0; i < pptr->numProcs; i++) {
-        insert(q, pptr->proc_arr[i]);
+    for(int i = 0; i < processes->numProcs; i++) {//Add all processes to q to start
+        insert(q, processes->proc_arr[i]);
     }
 
     print(q);
     
-    free(pptr->proc_arr);
+    //0-1024
+    //Start making algorithm for first
+    if (memStrat == 1) {
+        printf("Allocate by first\n");
+        printf("Current mem left for allocation = %d\n", ms->space_rem);
+        process node = PopMin(q);
+        process * head = &node;
+        ms->head = &head;
+        printf("sim pointer pointer points to pid %d and has memchunk %d\n", (*(ms->head))->pid, (*(ms->head))->memchunk);
+        printf("Node popped currentQ = %d\n", node.currentQ);
+        node = PopMin(q);
+        printf("Node popped currentQ = %d\n", node.currentQ);
+
+    }
+
+    free(q->arr);
+    free(q);
+    free(processes->proc_arr);
+    free(processes);
+    free(ms);
 }
