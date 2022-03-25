6b6495b2fe1a95af48c04e0053714202e887b9a3
taylor-dnblunt <taylor.dnblunt@gmail.com>
2022-03-24 20:54:48 -0700

    turned process struct into LL node, made proc_list struct, and made a sim struct for overall simulation

--- holes.h
+++ holes.h
@@ -4,10 +4,18 @@
 
 typedef struct process {
     int pid;
+    int currentQ;
     int memchunk;
+    int start;
+    int end;
+    int numSwaps;
+    struct process * next;
+} process;
+
+typedef struct proc_list {
     int numProcs;
     struct process * proc_arr;
-} process;
+} proc_list;
 
 struct Heap{
     process *arr;
@@ -16,6 +24,11 @@ struct Heap{
     int heap_type; // for min heap , 1 for max heap
 };
 typedef struct Heap Heap;
+
+typedef struct sim {
+    int space_rem; //Remaining space for allocation
+    process ** head;
+} sim;
 
 int inputChecker(int argc, char * argv[]);
 
